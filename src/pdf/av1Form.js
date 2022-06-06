9659f5fe589de1afb67b858ad79327e9c2f49c91
SabirovaOlya <sabirovaolya44@gmail.com>
2022-06-06 14:35:12 +0500

    06.06 second

--- src/pdf/av1Form.js
+++ src/pdf/av1Form.js
@@ -3,7 +3,22 @@ import './pdf.css'
 
 function av1Form() {
   return (
-    <div>av1Form</div>
+    <div className='print_container'>
+        <div className='av1_form_main'>
+            <div className='row_div'>
+                <p>Mikrokredit tashkilotining nomi</p>
+                <p>Renesans Mikrokredit Tashkiloti MChJ</p>
+            </div>
+            <div className='row-div'>
+                <div className='column_div'>
+                    <p>Mazkur varaqa kim tomondan to'ldirilgan</p>
+                    <p>(F.I.O. va mikrokredit tashkiloti mutaxassisining lavozimi)</p>
+                </div>
+                <p>Abdurashidova Dildora Asadulla qizi</p>
+            </div>
+            
+        </div>
+    </div>
   )
 }
 
